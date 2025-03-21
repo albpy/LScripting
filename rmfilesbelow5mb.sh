2e0c2cbdf24becea86d3137e9f4c2e4aaadce824
Albin Jose <albinjos64@gmail.com>
2025-03-21 21:14:48 +0530

    Delete rmfilesbelow5mb.sh

--- rmfilesbelow5mb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for file in /storage/emulated/0/Music/Recordings/Call\ Recordings/*; do
-    if test -f "$file" && [ $(stat -c%s "$file") -lt 5242880 ]; then
-        rm "$file"
-    fi
-done
-
